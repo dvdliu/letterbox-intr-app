5b785bb8b2487008a9273e91878c3bcabfb1a18d
David Liu <dliu10255@gmail.com>
2026-09-09 13:21:25 -0700

    update user data to sync with letterboxd profiles

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 import UserModal from "../models/user.js";
 
@@ -26,24 +27,50 @@ export const signin = async (req, res) => {
 };
 
 export const signup = async (req, res) => {
-  const { email, password, firstName, lastName } = req.body;
+  const { email, password, firstName, lastName, letterboxdUsername } = req.body;
 
   try {
     const oldUser = await UserModal.findOne({ email });
 
     if (oldUser) return res.status(400).json({ message: "User already exists" });
-    
-    
+
+
     const hashedPassword = await bcrypt.hash(password, 12);
 
-    const result = await UserModal.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` });
+    const result = await UserModal.create({
+      email,
+      password: hashedPassword,
+      name: `${firstName} ${lastName}`,
+      letterboxdUsername: letterboxdUsername || "",
+    });
 
     const token = jwt.sign( { email: result.email, id: result._id }, secret, { expiresIn: "1h" } );
 
     res.status(201).json({ result, token });
   } catch (error) {
     res.status(500).json({ message: "Something went wrong" });
-    
+
     console.log(error);
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  const { id } = req.params;
+  const { letterboxdUsername } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No user with id: ${id}`);
+
+  if (req.userId !== id) return res.status(403).json({ message: "You can only update your own profile." });
+
+  try {
+    const updatedUser = await UserModal.findByIdAndUpdate(
+      id,
+      { letterboxdUsername },
+      { new: true }
+    );
+
+    res.status(200).json({ result: updatedUser });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
   }
 };
--- server/models/user.js
+++ server/models/user.js
@@ -5,6 +5,7 @@ const userSchema = mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
   id: { type: String },
+  letterboxdUsername: { type: String, default: '' },
 });
 
 export default mongoose.model("User", userSchema);
